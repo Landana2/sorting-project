578e377458968e8762cc1705b83b1857f8a66348
Landana2 <lleung0509@gmail.com>
2025-01-27 20:23:48 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,17 +2,18 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        int[] testInput = generateRandomArray(100, 1000);
-
-        TestSuite.run(testInput, 1);
+        int[] testInput = generateRandomArray(1000, 0, 1000);
+        TestSuite.run(testInput, 10);
     }
 
-    public static int[] generateRandomArray(int size, int maxValue) {
+    public static int[] generateRandomArray(int size, int minValue, int maxValue) {
         Random rand = new Random();
         int[] array = new int[size];
+
         for (int i = 0; i < size; i++) {
-            array[i] = rand.nextInt(maxValue);
+            array[i] = rand.nextInt((maxValue - minValue) + 1) + minValue;
         }
+
         return array;
     }
 }
